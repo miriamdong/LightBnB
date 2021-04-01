e46e01f5bf05e9dda4a19f96c8d799950580e7a7
miriamdong <miriamdong@gmai.com>
2021-04-01 05:36:47 +0000

    add 3 more filters

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,12 +91,12 @@ FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE reservations.guest_id = $1
-AND end_date < now()::date
+AND reservations.end_date < now()::date
 GROUP BY reservations.id, properties.id
-ORDER BY start_date
+ORDER BY reservations.start_date
 LIMIT $2;
 `, [guest_id, limit])
-    .then(res => res.rows[0])
+    .then(res => res.rows)
     .catch(err => console.error('query error', err.stack));
 };
 exports.getAllReservations = getAllReservations;
@@ -110,14 +110,61 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-SELECT *
-FROM properties
-LIMIT $1;
-`, [limit])
-    .then(res => res.rows)
-    .catch(err => console.error('query error', err.stack));
+
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${ options.city }%`);
+    queryString += `WHERE city LIKE $${ queryParams.length } `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${ options.owner_id }`);
+    queryString += `AND owner_id = $${ queryParams.length } ` || `WHERE owner_id = $${ queryParams.length } `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+
+    queryParams.push(`${options.minimum_price_per_night * 100}`, `${ options.maximum_price_per_night * 100}`);
+    queryString += `
+    AND cost_per_night <= $${ queryParams.length } AND cost_per_night >= $${ queryParams.length - 1 }` || `
+    WHERE cost_per_night <= $${ queryParams.length } AND cost_per_night >= $${ queryParams.length - 1 }`;
+  }
+
+
+
+  // 4
+  queryString += `
+  GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${ options.minimum_rating }`);
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${ queryParams.length } `;
+
+  }
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${ queryParams.length };
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows);
 };
+
 exports.getAllProperties = getAllProperties;
 
 
@@ -131,18 +178,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  return pool.query(`INSERT INTO properties
-  (owner_id, title, description,
-  thumbnail_photo_url , cover_photo_url,
-  cost_per_night, street, city,
-  province, post_code, country,
-  parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  return pool.query(`
+  INSERT INTO properties
+  (title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms,
+  province, city, country, street, post_code)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-  RETURNING *;`, [property.owner_id, property.title, property.description,
-      property.thumbnail_photo_url, property.cover_photo_url,
-      property.cost_per_night, property.street, property.city,
-      property.province, property.post_code, property.country,
-      property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms
+  RETURNING *;`, [property.title, property.description, property.owner_id,
+      property.cover_photo_url, property.thumbnail_photo_url,
+      property.cost_per_night, property.parking_spaces,
+      property.number_of_bathrooms, property.number_of_bedrooms,
+      property.province, property.city, property.country, property.street, property.post_code
     ])
     .then(res => res.rows[0])
     .catch(err =>
